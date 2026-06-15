13da91f75a36d168a00158752ec14afd86dbd619
pictureinthenoise <54972965+pictureinthenoise@users.noreply.github.com>
2026-06-15 13:46:48 -0700

    Update run_infer.sh to include CKPT_LORA_PATH

--- scripts/run_infer.sh
+++ scripts/run_infer.sh
@@ -13,12 +13,14 @@ export HF_ENDPOINT=https://hf-mirror.com
 export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
 CKPT_PATH=$1
-OUT_CSV=$2
-TEST_ROOT=$3
-CONFIG_PATH=${4:-configs/whisper_qwen0_6b_constrained_event_formal_5labels_competition.yaml}
+CKPT_LORA_PATH=$2
+OUT_CSV=$3
+TEST_ROOT=$4
+CONFIG_PATH=${5:-configs/whisper_qwen0_6b_constrained_event_formal_5labels_competition.yaml}
 CKPT_PATH="${CKPT_PATH//$'\r'/}"
+CKPT_LORA_PATH="${CKPT_LORA_PATH//$'\r'/}"
 OUT_CSV="${OUT_CSV//$'\r'/}"
 TEST_ROOT="${TEST_ROOT//$'\r'/}"
 CONFIG_PATH="${CONFIG_PATH//$'\r'/}"
 
-python3 -m src.infer_test --config "${CONFIG_PATH}" --checkpoint "${CKPT_PATH}"  --test_root "${TEST_ROOT}" --output_csv "${OUT_CSV}"
+python3 -m src.infer_test --config "${CONFIG_PATH}" --checkpoint "${CKPT_PATH}" --checkpoint_lora "${CKPT_LORA_PATH}" --test_root "${TEST_ROOT}" --output_csv "${OUT_CSV}"
